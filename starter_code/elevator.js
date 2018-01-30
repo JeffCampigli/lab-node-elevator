50397a79c3d071ce1e2f909cf9a9686474d7088e
Jeff Campigli <jfcampigli@gmail.com>
2018-01-30 09:11:38 +0100

    PAR Jeff

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,39 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = "";
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    var interval = setInterval(() => {
+      this.update();
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(interval);
+  }
+
+  update() {
+    this.log(this.direction, this.floor);
+  }
+
+  _passengersEnter() {}
+  _passengersLeave() {}
+  floorUp() {
+    this.floor === this.MAXFLOOR ? console.log("dernier etage") : this.floor++,
+      (this.direction = "up");
+  }
+  floorDown() {
+    this.floor === 0 ? console.log("Rez de Chausée") : this.floor--,
+      (this.direction = "down");
+  }
+  call() {}
+  log(direction, position) {
+    console.log(`Direction: ${direction} | Floor: ${position}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,10 @@
-const Elevator = require('./elevator.js');
+const Elevator = require("./elevator.js");
+
+var elevator = new Elevator();
+elevator.floorUp();
+elevator.floorUp();
+elevator.floorUp();
+elevator.floorUp();
+elevator.floorUp();
+elevator.floorUp();
+elevator.update();
